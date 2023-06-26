f981e6fc5d2bb9b8a77ec1d73cba47bc71aa3baf
L-Karimi <luciahkei101@gmail.com>
2023-06-26 13:53:24 +0300

    mega commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Terms_of_service from "./Pages/Terms_of_service";
+import About from "./Pages/About";
+import Privacy_policy from "./Pages/Privacy_policy";
+import Services from "./Pages/Services";
+import Home from "./Pages/Home";
+import Insights from "./Pages/Insights";
 
 function App() {
   return (
     <>
       <div>
-        ReactDOM.render(
         <Router>
           <Navbar />
-          {/* Other components and content */}
+          <Routes>
+            <Route path="/Home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/insights" element={<Insights />} />
+            <Route path="/Terms_of_service" element={<Terms_of_service />} />
+            <Route path="/Privacy_policy" element={<Privacy_policy />} />
+          </Routes>
         </Router>
-        , document.getElementById("root")
       </div>
-      );
     </>
   );
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Player } from "react-html5video";
+import vedio2 from "./../Vedio/introductrory.mp4";
+const Home = () => {
+  return (
+    <div>
+      {/* <Player autoPlay loop muted controls={false} poster="path_to_poster_image.jpg">
+        <source src={vedio2} type="video/mp4" />
+      </Player> */}
+    </div>
+  );
+};
+
+export default Home;
